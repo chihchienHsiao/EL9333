6c3a50981594b7b994a7a85759e02e4774cd5db2
J Cai <jc8123@nyu.edu>
2018-05-13 16:16:13 -0400

    add default UDP flow

--- SimpleMonitor13.py
+++ SimpleMonitor13.py
@@ -3,7 +3,7 @@ from operator import attrgetter
 
 from ryu.app import simple_switch_13
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, HANDSHAKE_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
 
@@ -17,8 +17,57 @@ class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
         super(SimpleMonitor13, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
-	self.tempTraffic = [0] * 15
-	self.linkTraffic = []	
+        self.tempTraffic = [0] * 15
+        self.linkTraffic = []	
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid = datapath.id
+        # if dpid == 1 or dpid == 3 or dpid == 4 or dpid == 5:
+        #     self.logger.debug("add datapath %d" %dpid)
+        #     self.datapaths[datapath.id] = datapath
+
+        # install the table-miss flow entry.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+        # install default rule (UDP)
+        for eth_dst in range(1,4):
+            if dpid == 3 or dpid == 4 or dpid == 5:
+                if dpid == eth_dst+2:
+                    match = parser.OFPMatch(
+                        eth_type=0x800,
+                        eth_dst='00:00:00:00:00:0%d'%eth_dst,
+                        in_port=2,
+                        ip_proto=17
+                    )
+                    actions = [parser.OFPActionOutput(1)]
+                    self.add_flow(datapath, 5, match, actions)
+                else:
+                    match = parser.OFPMatch(
+                        eth_type=0x800,
+                        eth_dst='00:00:00:00:00:0%d'%eth_dst,
+                        in_port=1,
+                        ip_proto=17
+                    )
+                    actions = [parser.OFPActionOutput(2)]
+                    self.add_flow(datapath, 5, match, actions)
+            elif dpid == 1:
+                for in_port in range(1,4):
+                    if(in_port!=eth_dst):
+                        match = parser.OFPMatch(
+                            eth_type=0x800,
+                            eth_dst='00:00:00:00:00:0%d'%eth_dst,
+                            in_port = in_port,
+                            ipv4_dst='10.0.0.1'
+                        )
+                        actions = [parser.OFPActionOutput(1)]
+                        self.add_flow(datapath, 5, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -84,12 +133,12 @@ class SimpleMonitor13(simple_switch_13.SimpleSwitch13):
                              ev.msg.datapath.id, stat.port_no,
                              stat.rx_packets, stat.rx_bytes, stat.rx_errors,
                              stat.tx_packets, stat.tx_bytes, stat.tx_errors)
-	    if stat.port_no <= 3:
-	    	idx = (stat.port_no + (ev.msg.datapath.id - 1) * 3) - 1
-	    self.tempTraffic[idx] += stat.rx_bytes
-	    self.linkTraffic.append(self.tempTraffic)
-	    self.logger.info("??????? %d", stat.rx_bytes)
-	    print(self.linkTraffic)
+        if stat.port_no <= 3:
+            idx = (stat.port_no + (ev.msg.datapath.id - 1) * 3) - 1
+        self.tempTraffic[idx] += stat.rx_bytes
+        self.linkTraffic.append(self.tempTraffic)
+        self.logger.info("??????? %d", stat.rx_bytes)
+        print(self.linkTraffic)
 
 
 
